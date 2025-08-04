d1c1a3eb893ee2324789077a5e186566ad1cb2de
uzhojiakbar <uzhojiakbar3@gmail.com>
2025-08-05 00:10:02 +0500

    create project

---
+++ src/components/Navbar/index.jsx
@@ -0,0 +1,11 @@
+import { Container } from "./style"
+
+const Navbar = () => {
+    return (
+        <Container>
+            <h1>Navbar</h1>
+        </Container>
+    )
+}
+
+export default Navbar
---
+++ src/components/Sidebar/index.jsx
@@ -0,0 +1,11 @@
+import { Container } from "./style"
+
+const Sidebar = () => {
+    return (
+        <Container>
+            <h1>Sidebar</h1>
+        </Container>
+    )
+}
+
+export default Sidebar
